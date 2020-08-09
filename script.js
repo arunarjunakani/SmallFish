eba86e8a2c65f1eae5f6a75899734bd63a96dd20
arunarjunakani <arunarjunakani@gmail.com>
2020-08-09 14:56:43 -0500

    Connected move validation to GUI

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-var ruyLopez = 'r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R'
-var board = Chessboard('myBoard', {
-	position: ruyLopez
+function onChange(oldPos, newPos) {
+	// console.log(game.moves())
+	if (game.game_over())
+		console.log("Game Over")
+}
+
+const game = new Chess()
+
+function onDrop(start, end) {
+	move = game.move({
+		from: start,
+		to: end,
+		promotion: 'q'
+	})
+	if (move == null) return 'snapback'
+}
+
+const board = Chessboard('myBoard', {
+	draggable: true,
+	onChange: onChange,
+	onDrop: onDrop,
+	showNotation: false,
+	position: 'start'
 });
+
+
 
